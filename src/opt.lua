091cec74fa9a1f5492fd8e4e5de74e311b8f3d2f
mattmc3 <mattmc3@gmail.com>
2025-07-26 09:15:48 -0400

    Refactor

--- src/opt.lua
+++ src/opt.lua
@@ -27,6 +27,87 @@ function OptSet:arg(name, handler)
 	self.cfg[name] = new_opt(name, true, handler)
 end
 
+function OptSet:long_opt(name, handler)
+	self.cfg["--" .. name] = new_opt("--" .. name, false, function(_, _) return handler("--" .. name) end)
+end
+
+function OptSet:long_arg(name, handler)
+	self.cfg["--" .. name] = new_opt("--" .. name, true, handler)
+end
+
+local function handle_group(self, group, args, i, fallback)
+	local j = 1
+	while j <= #group do
+		local ch = group:sub(j, j)
+		local cfg = self.cfg[ch]
+		if not cfg then
+			local err = fallback("-" .. ch)
+			if err then return err, i end
+		elseif cfg.has_arg then
+			local val
+			if j < #group then
+				val = group:sub(j + 1)
+				if val:sub(1,1) == "=" then
+					val = val:sub(2)
+				elseif #val > 0 then
+					return "missing argument for -" .. ch, i
+				end
+				j = #group
+			elseif i <= #args then
+				val = args[i]
+				i = i + 1
+			else
+				return "missing argument for -" .. ch, i
+			end
+			local err = cfg.handler("-" .. ch, val)
+			if err then return err, i end
+			break
+		else
+			local err = cfg.handler("-" .. ch, "")
+			if err then return err, i end
+		end
+		j = j + 1
+	end
+	return nil, i
+end
+
+local function handle_long(self, arg, args, i, fallback)
+	local eq = arg:find("=")
+	local name, val
+	if eq then
+		name = arg:sub(3, eq - 1)
+		val = arg:sub(eq + 1)
+	else
+		name = arg:sub(3)
+	end
+	local cfg = self.cfg["--" .. name]
+	if not cfg then
+		-- Pass unknown long option to fallback
+		local err = fallback(arg)
+		if err then return err, i end
+		return nil, i
+	end
+	if cfg.has_arg then
+		if val ~= nil then
+			local err = cfg.handler("--" .. name, val)
+			if err then return err, i end
+		else
+			if i <= #args then
+				val = args[i]
+				i = i + 1
+				local err = cfg.handler("--" .. name, val)
+				if err then return err, i end
+			else
+				return "missing argument for --" .. name, i
+			end
+		end
+	else
+		local err = cfg.handler("--" .. name, "")
+		if err then return err, i end
+	end
+	return nil, i
+end
+
 -- Parse CLI args
 function OptSet:parse(args, fallback)
 	local i = 1
@@ -44,46 +125,16 @@ function OptSet:parse(args, fallback)
 					i = i + 1
 				end
 				break
+			else
+				local err
+				err, i = handle_long(self, arg, args, i, fallback)
+				if err then return err end
 			end
-			-- No support for long options in this version
-			return "unsupported long option: " .. arg
-
 		elseif arg:sub(1, 1) == "-" and #arg > 1 then
 			local group = arg:sub(2)
-			local j = 1
-			while j <= #group do
-				local ch = group:sub(j, j)
-				local cfg = self.cfg[ch]
-				if not cfg then
-					local err = fallback("-" .. ch)
-					if err then return err end
-				elseif cfg.has_arg then
-					local val
-					if j < #group then
-						val = group:sub(j + 1)
-						-- Handle -o=val
-						if val:sub(1,1) == "=" then
-							val = val:sub(2)
-						elseif #val > 0 then
-							-- If there are extra chars after the option, that's an error
-							return "missing argument for -" .. ch
-						end
-						j = #group
-					elseif i <= #args then
-						val = args[i]
-						i = i + 1
-					else
-						return "missing argument for -" .. ch
-					end
-					local err = cfg.handler("-" .. ch, val)
-					if err then return err end
-					break -- argument-consuming options must be last in group
-				else
-					local err = cfg.handler("-" .. ch, "")
-					if err then return err end
-				end
-				j = j + 1
-			end
+			local err
+			err, i = handle_group(self, group, args, i, fallback)
+			if err then return err end
 		else
 			-- Positional
 			local err = fallback(arg)
